01c1c05d0342d790c27ddf62f943832782e81533
vthakkar27 <dasvthakkar27@gmail.com>
2020-06-12 23:44:53 +0100

    switch_statment

---
+++ IdeaProjects/switch_statment/src/big_number.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class big_number {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        int x = scanner.nextInt();
+        System.out.print("Enter the second number:");
+        int y = scanner.nextInt();
+        System.out.print("Enter the third number:");
+        int z = scanner.nextInt();
+
+        if(x > y && x > z)
+        {
+            System.out.println("Largest number is:"+x);
+        }
+        else if(y > z)
+        {
+            System.out.println("Largest number is:"+y);
+        }
+        else
+        {
+            System.out.println("Largest number is:"+z);
+        }
+
+    }
+
+}
